cbfe95b17bbe8710b6ab6ed67073dc6a9d3eff89
Max Rupplin <mearvk@outlook.com>
2026-05-13 17:31:31 -0400

    US Calendar Whites

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -12,6 +12,8 @@ public class Calendar
 
     public static final String PUBLIC_SAFETY_KEYS = "66FeCFE s BEFS Blue";          //All Public Officials should have and maintain an Area of Safe Public Address all the Way into the 66th Month of the Years - February
 
+    public static final String FIGHTING_NEWS = "10.4x Actual Cancelled apparent Reasons elsewheres.";
+
     protected TimeZone timezone = null;
 
     public Calendar(TimeZone timezone)
