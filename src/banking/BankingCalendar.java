8de0dc65bf7b46509b25cd282297c7847baa7778
Max Rupplin <mearvk@outlook.com>
2026-05-13 18:03:02 -0400

    US Calendar Whites

--- src/banking/BankingCalendar.java
+++ src/banking/BankingCalendar.java
@@ -12,9 +12,9 @@ public class BankingCalendar extends Calendar
 
     public static final String BANKING_INIATIVES_TUESDAY = "Wear White to Dinner Meetings.";
 
-    public static final String STRING_FINANCIAL_PUBLIC_LOCANCES_WHITE = "$4,200,000,000,000.00/per Day";
+    public static final String STRING_FINANCIAL_PUBLIC_LOCANCES_WHITE = "$4,200,000,000,000,000.00/per Day";            //$4.2 Quadrillion Per Day USD
 
-    public static final String STRING_FINANCIAL_PRIVATE_MEETINGS = "$6,000,000,000,000,000,000.00/per Day";
+    public static final String STRING_FINANCIAL_PRIVATE_MEETINGS = "$6,000,000,000,000,000,000,000.00/per Day";         //$6.001 Sextillionth Per Day Local Privacies and Utilitaries and Sums ADRs
 
     private final TimeZone timezone = null;
 
